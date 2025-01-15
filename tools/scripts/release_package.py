82fd14bb879ad39a13b88172e615d0f40422524a
Mathias Dam Arvanaghi <mathias.dam.arvanaghi@laerdal.com>
2025-01-15 10:46:33 +0100

    ci: origin url

--- tools/scripts/release_package.py
+++ tools/scripts/release_package.py
@@ -110,7 +110,8 @@ def _commit_and_tag(repo: git.Repo, version: str):
 def _push(repo: git.Repo):
     origin = repo.remote()
     index = origin.url.find("https://")
-    origin.set_url(origin.url[:index] + os.environ["GITHUB_TOKEN"] + "@" + origin.url[index:])
+    origin.set_url(origin.url[:index + 1] + os.environ["GITHUB_TOKEN"] + "@" + origin.url[index + 1:])
+    print(f"Pushing to {origin.url}")
     origin.push()
 
 def _add_repo_note(dest: str):
